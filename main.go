19a4586779f33b655717f6a6116cb95710743c3f
Shannon Lucas <shannonlucas4@gmail.com>
2021-08-08 16:28:02 -0700

    add release notes for ref back later

--- main.go
+++ main.go
@@ -5,3 +5,5 @@ import "github.com/lucassha/vault-traverser/cmd"
 func main() {
 	cmd.Execute()
 }
+
+// release notes on the upgrade to kv v2 in vault 0.10: https://www.hashicorp.com/blog/vault-0-10
