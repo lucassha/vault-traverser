e3389b42d46646cbce570b778bf8eacfbc79eeb6
Shannon Lucas <shannonlucas4@gmail.com>
2021-08-09 13:35:40 -0700

    add cluster helper function

--- vault/vault_test.go
+++ vault/vault_test.go
@@ -3,6 +3,11 @@ package vault
 import (
 	"testing"
 
+	kv "github.com/hashicorp/vault-plugin-secrets-kv"
+	"github.com/hashicorp/vault/api"
+	vaulthttp "github.com/hashicorp/vault/http"
+	"github.com/hashicorp/vault/sdk/logical"
+	hashivault "github.com/hashicorp/vault/vault"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,4 +52,101 @@ func TestMutateReadSecretAPIPath(t *testing.T) {
 			assert.Equal(t, tc.want, got, "these paths should be the same")
 		})
 	}
+}
+
+// CreateTestVault spins up a Vault server and tests against
+// an actual Vault instance. Currently, this is only set up for
+// kv v2. Mostly copied from this github issue:
+// https://github.com/hashicorp/vault/issues/8440
+func createTestVault(t testing.TB) *hashivault.TestCluster {
+	t.Helper()
+
+	// CoreConfig parameterizes the Vault core config
+	coreConfig := &hashivault.CoreConfig{
+		LogicalBackends: map[string]logical.Factory{
+			"kv": kv.Factory,
+		},
+	}
+
+	cluster := hashivault.NewTestCluster(t, coreConfig, &hashivault.TestClusterOptions{
+		// Handler returns an http.Handler for the API. This can be used on
+		// its own to mount the Vault API within another web server.
+		HandlerFunc: vaulthttp.Handler,
+	})
+	cluster.Start()
+
+	// Create KV V2 mount on the path /test
+	// It starts in cluster mode, so you just pick one of the three clients
+	// In this case, Cores[0] is just always picking the first one
+	if err := cluster.Cores[0].Client.Sys().Mount("test", &api.MountInput{
+		Type: "kv",
+		Options: map[string]string{
+			"version": "2",
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	return cluster
+}
+
+func TestReadSecret(t *testing.T) {
+	cluster := createTestVault(t)
+	defer cluster.Cleanup()
+	vaultClient := cluster.Cores[0].Client // only need a client from 1 of 3 clusters
+
+	// instead of using NewVaultClient(engine string) constructor,
+	// pass in the test client into the VaultClient struct
+	vc := &VaultClient{
+		client:   vaultClient,
+		kvEngine: "v2",
+	}
+
+	// write sample data into vault
+	testData := []struct {
+		path  string
+		key   string
+		value string
+	}{
+		{"test/data/test0", "test_0_key", "test_0_data"},
+		{"test/data/test1", "test_1_key", "test_1_data"},
+		{"test/data/test2", "test_2_key", "test_2_data"},
+	}
+
+	for _, v := range testData {
+		_, err := vc.client.Logical().Write(v.path, map[string]interface{}{
+			"data": map[string]interface{}{
+				v.key: v.value,
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	testTable := []struct {
+		name     string
+		endpoint string
+		key      string
+		want     []string
+	}{
+		{"test case 0", "test/test0", "test_0_key", []string{"test_0_data"}},
+		{"test case 0", "test/test123", "test_0_key", []string{"test_0_data"}},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.name, func(t *testing.T) {
+			secrets, err := vc.readSecret(tc.endpoint)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for i := 0; i < len(secrets); i++ {
+				if secrets[i] != tc.want[i] {
+					t.Errorf("got %s but wanted %s", secrets[i], tc.want[i])
+				}
+			}
+		})
+	}
+
 }
